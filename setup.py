d4f07d941e0dfbb98a244dd28ed766f559b5a92a
jvanleersum <75987870+jvanleersum@users.noreply.github.com>
2021-10-20 15:02:38 +0200

    'initialcommit'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='titanic_survival_ml',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/titanic_survival_ml-run'],
+      zip_safe=False)
