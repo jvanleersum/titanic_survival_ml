26c316fc4ae95aaf09dfe8e8cbdecba9abc3dfe4
jvanleersum <75987870+jvanleersum@users.noreply.github.com>
2021-11-19 14:48:26 +0100

    Updated api with predict endpoint

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from titanic_survival_ml.predict import get_model, make_prediction
 
 app = FastAPI()
 
@@ -14,3 +16,26 @@ app.add_middleware(
 @app.get("/")
 def index():
     return {"greeting": "Hello world"}
+
+@app.get("/predict")
+def predict_survival(pclass, sex, age, sibsp, parch, fare, embarked, title):
+    family = sibsp + parch
+    X_pred = pd.DataFrame({
+            "Pclass": pd.Series(pclass, dtype='float64'),
+            "Sex": pd.Series(sex, dtype='object'),
+            "Age": pd.Series(age, dtype='float64'),
+            "SibSp": pd.Series(sibsp, dtype='float64'),
+            "Parch": pd.Series(parch, dtype="float64"),
+            "Fare": pd.Series(fare, dtype="float64"),
+            "Embarked": pd.Series(embarked, dtype='object'),
+            "Title": pd.Series(title, dtype='object'),
+            "Family": pd.Series(family, dtype="float64")
+            })
+    result, survival_probability = make_prediction(X_pred)
+    if result == 1:
+        prediction = 'Survived'
+    else:
+        prediction = 'Not survived'
+    return {"prediction": prediction, 
+            "survival_probability": survival_probability}
+    
